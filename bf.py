6db73377a07e4b0cbafad3b5cc5811b7b7fbb78e
mmlab <mmlab.cs.aueb@gmail.com>
2018-03-12 02:59:04 +0200

    Compining proxy with BF

--- bf.py
+++ bf.py
@@ -26,6 +26,9 @@ class BFServer:
                 ip_packet = ether.payload 
                 if ip_packet.dstip == IPAddr("192.168.130.200"):
                    self.handler.handle_packet(ip_packet.payload)
+    def nb_listen(self):
+        threading.Thread(target = self.listen)
+        
 class BFClient:
     def __init__(self):
         self.s = socket(AF_PACKET, SOCK_RAW)
@@ -39,7 +42,6 @@ class BFClient:
         ether             = ethernet()
         ether.type        = 0x0800
         ether.dst         = EthAddr(b"\x00\x00\x00\x00\x00"+chr(bf))
-        #ether.src         = EthAddr(b"\xfa\x16\x3e\xae\xdc\xa6")
         ether.src         = EthAddr(b"\xfa\x16\x3e\xae\xdc\x00")
         ether.payload     = ip_packet
         self.s.send(ether.pack())
