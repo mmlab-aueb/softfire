7c81a17ebb3f07d6982f9d34769ac424e7a64d9f
mmlab <mmlab.cs.aueb@gmail.com>
2018-03-12 03:07:09 +0200

    Compining proxy with BF

--- test-bf-client.py
+++ test-bf-client.py
@@ -1,6 +1,7 @@
 from bf import BFClient
 
 client = BFClient()
-for x in (1,2,4,8,3,5,9,6,10,12,7,11,13,14,15):
-    payload = " Bloom filrer = " + '%x'%x
-    client.send_packet(x,payload)
+#for x in (1,2,4,8,3,5,9,6,10,12,7,11,13,14,15):
+x=0
+payload = " Bloom filrer = " + '%x'%x
+client.send_packet(x,payload)
