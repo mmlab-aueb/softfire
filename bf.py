48448f4b1c3d18e0adec30d399834b74e40a957b
mmlab <mmlab@cs.aueb.gr>
2018-03-12 02:21:52 +0100

    Threads

--- bf.py
+++ bf.py
@@ -25,10 +25,9 @@ class BFServer:
             if ether.type == 0x0800: #IP
                 ip_packet = ether.payload 
                 if ip_packet.dstip == IPAddr("192.168.130.200"):
-                    print "Received packet"
                     self.handler.handle_packet(ip_packet.payload)
     def nb_listen(self):
-        threading.Thread(target = self.listen).strart()
+        threading.Thread(target = self.listen).start()
         
 class BFClient:
     def __init__(self):
